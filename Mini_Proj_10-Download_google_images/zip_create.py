283175d602146adf49ee143884e1815db3cdde0c
Vatsal Nanda <54484624+VatsalNanda@users.noreply.github.com>
2021-08-16 22:29:26 +0530

    Delete zip_create.py

--- Mini_Proj_10-Download_google_images/zip_create.py
+++
@@ -1,9 +0,0 @@
-import zipfile
-import os
-from resize import resized_dir
-folder_to_be_zipped = resized_dir
-filename=input('Enter the filename fir zip')
-with zipfile.ZipFile(filename+'.zip', 'w', zipfile.ZIP_DEFLATED) as newzip:
-    for dirpath, dirnames, files in os.walk(folder_to_be_zipped):
-        for file in files:
-            newzip.write(os.path.join(dirpath, file))
